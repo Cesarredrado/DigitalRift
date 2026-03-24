97b1f65b68ed48373e5f7438117a72364fbab934
cesarredrado <redrado.cesar@gmail.com>
2026-03-24 22:29:31 +0100

    Footer y contacto

--- translations.js
+++ translations.js
@@ -11,6 +11,11 @@ const translations = {
     'index.proyecto': 'DigitalRift',
     'index.description': 'El proyecto DigitalRift busca comprender las implicaciones ambientales y socioeconómicas de las políticas públicas de Digitalización de la Agricultura.',
     'index.marcoLogico': '/MarcoLogico.drawio.png',
+    'index.contactTitle': 'Contacto',
+    'index.contactDescription': 'Para consultas del proyecto, escríbenos directamente por correo.',
+
+    // Footer
+    'footer.copyright': '© 2026 DigitalRift Project - IEGD-CSIC. Todos los derechos reservados.',
     
     // Quiénes somos
     'quienes.titulo': 'Quiénes Somos',
@@ -67,6 +72,11 @@ const translations = {
     'index.proyecto': 'DigitalRift',
     'index.description': 'The DigitalRift project seeks to understand the environmental and socio-economic implications of Public policies for the Digitalisation of Agriculture.',
     'index.marcoLogico': '/LogicalFramework.drawio.png',
+    'index.contactTitle': 'Contact',
+    'index.contactDescription': 'For project-related questions, contact us directly by email.',
+
+    // Footer
+    'footer.copyright': '© 2026 DigitalRift Project - IEGD-CSIC. All rights reserved.',
     
     // Who are we
     'quienes.titulo': 'Who are we?',
